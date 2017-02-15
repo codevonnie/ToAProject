28ba328c216831b534d5a87435ffc07a32dc45b7
Yvonne Grealy <yvonnegrealy@hotmail.com>
2017-02-15 19:20:45 +0000

    Code testing

--- numbersGame.rkt
+++ numbersGame.rkt
@@ -3,3 +3,27 @@
 ; Theory of Algorithms 4th Year Project
 ; Author Yvonne Grealy
 
+(define ops '(+ - * /))
+(define a 5)
+(define b 25)
+(define t 125)
+;(define l '(
+;           (+ a b)
+;           (+ b a)
+;           (- a b)
+;           (- b a)
+;           (* a b)
+;           (* b a)
+;           (/ a b)
+;           (/ b a)
+;           ))
+
+(+ a b)
+(+ b a)
+(- a b)
+(- b a)
+(* a b)
+(* b a)
+(/ a b)
+(/ b a)
+  
