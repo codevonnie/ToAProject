be2a673d1db4e232d75214660ee77f8cc7cc77b2
Yvonne Grealy <yvonnegrealy@hotmail.com>
2017-02-24 16:43:41 +0000

    Combinations, Permutations, Cartesian Product
    - Figuring out what is returned from using different conbinations of the
    above methods

--- numbersGame.rkt
+++ numbersGame.rkt
@@ -4,10 +4,15 @@
 ; Author Yvonne Grealy
 
 (define ops '(+ - * /))
+(define nums (list 5 25))
 (define a 5)
 (define b 25)
 (define t 125)
-;(define l '(
+
+
+; Get all permutations of list of 2 with operator
+; ********* INFINTE LOOP **************
+;(permutations (list
 ;           (+ a b)
 ;           (+ b a)
 ;           (- a b)
@@ -18,12 +23,71 @@
 ;           (/ b a)
 ;           ))
 
-(+ a b)
-(+ b a)
-(- a b)
-(- b a)
-(* a b)
-(* b a)
-(/ a b)
-(/ b a)
-  
+; print out all possible combinations hard coded
+(define something (list
+           (+ a b)
+           (+ b a)
+           (- a b)
+           (- b a)
+           (* a b)
+           (* b a)
+           (/ a b)
+           (/ b a)
+           ))
+;something
+
+
+; prints out '((
+;  (+)
+;  (-)
+;  (+ -)
+;  (*)
+;  (+ *)
+;  (- *)
+;  (+ - *)
+;  (/)
+;  (+ /)
+;  (- /)
+;  (+ - /)
+;  (* /)
+;  (+ * /)
+;  (- * /)
+;  (+ - * /))
+;(combinations ops)
+
+; '((5 25))
+;(combinations nums 2)
+
+; prints out '((+ 5) (+ 25) (- 5) (- 25) (* 5) (* 25) (/ 5) (/ 25))
+(define all (cartesian-product ops nums))
+;all
+
+; ********* INFINTE LOOP **************
+;(permutations all)
+
+; prints out '((5 25) (25 5))
+(define num (permutations (list 5 25)))
+ ;num
+
+; prints out '((+ (5 25)) (+ (25 5)) (- (5 25)) (- (25 5)) (* (5 25)) (* (25 5)) (/ (5 25)) (/ (25 5)))
+;(cartesian-product ops num)
+
+; prints out '((+ 5 5)
+;  (+ 5 25)
+;  (+ 25 5)
+;  (+ 25 25)
+;  (- 5 5)
+;  (- 5 25)
+;  (- 25 5)
+;  (- 25 25)
+;  (* 5 5)
+;  (* 5 25)
+;  (* 25 5)
+;  (* 25 25)
+;  (/ 5 5)
+;  (/ 5 25)
+;  (/ 25 5)
+;  (/ 25 25))
+(define allCart (cartesian-product ops (list 5 25) (list 5 25)))
+allCart
+
