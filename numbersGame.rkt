de2e26d514451fc5aeb83eb44aecf29658d44034
Yvonne Grealy <yvonnegrealy@hotmail.com>
2017-02-27 15:51:47 +0000

    Using eval to get values from list of lists
    - Getting cartesian-product of the possible combinations of numbers and
    operators stores each calculation in a list of lists. Sending this to
    function that takes the car of the list and than evaluates it gets the
    correct result
    - Requires recursion but eval only works on the first car

--- numbersGame.rkt
+++ numbersGame.rkt
@@ -5,6 +5,8 @@
 
 (define ops '(+ - * /))
 (define nums (list 5 25))
+(define 6List '( ))
+(define numList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 (define a 5)
 (define b 25)
 (define t 125)
@@ -67,7 +69,7 @@
 
 ; prints out '((5 25) (25 5))
 (define num (permutations (list 5 25)))
- ;num
+ num
 
 ; prints out '((+ (5 25)) (+ (25 5)) (- (5 25)) (- (25 5)) (* (5 25)) (* (25 5)) (/ (5 25)) (/ (25 5)))
 ;(cartesian-product ops num)
@@ -89,5 +91,21 @@
 ;  (/ 25 5)
 ;  (/ 25 25))
 (define allCart (cartesian-product ops (list 5 25) (list 5 25)))
-allCart
+;allCart
+
+
+; define namespace to use eval on allCart list
+(define ns (make-base-namespace))
+
+(define (getList l)
+  (if (null? l)
+      1
+      (car l)))
+
+; prints 10 which is first calculation - need to work on recursion
+(eval (getList allCart) ns)
+
+;(getList allCart)
+;(eval (getList allCart))
+;(pair? allCart)
 
