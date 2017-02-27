f3190b7d1b51d1688ab48cf82d06213b59ddd45e
Yvonne Grealy <yvonnegrealy@hotmail.com>
2017-02-27 17:58:22 +0000

    Function to check if target is in answers list
    - checkTarget function goes through list of answers and checks if target
    exists.  Compiles list of true and false values

--- numbersGame.rkt
+++ numbersGame.rkt
@@ -5,7 +5,6 @@
 
 (define ops '(+ - * /))
 (define nums (list 5 25))
-(define 6List '( ))
 (define numList (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 (define a 5)
 (define b 25)
@@ -14,47 +13,14 @@
 
 ; Get all permutations of list of 2 with operator
 ; ********* INFINTE LOOP **************
-;(permutations (list
-;           (+ a b)
-;           (+ b a)
-;           (- a b)
-;           (- b a)
-;           (* a b)
-;           (* b a)
-;           (/ a b)
-;           (/ b a)
-;           ))
+;(permutations (list (+ a b)(+ b a)(- a b)(- b a)(* a b)(* b a)(/ a b)(/ b a)))
 
 ; print out all possible combinations hard coded
-(define something (list
-           (+ a b)
-           (+ b a)
-           (- a b)
-           (- b a)
-           (* a b)
-           (* b a)
-           (/ a b)
-           (/ b a)
-           ))
+(define something (list (+ a b)(+ b a)(- a b)(- b a)(* a b)(* b a)(/ a b)(/ b a)))
 ;something
 
 
-; prints out '((
-;  (+)
-;  (-)
-;  (+ -)
-;  (*)
-;  (+ *)
-;  (- *)
-;  (+ - *)
-;  (/)
-;  (+ /)
-;  (- /)
-;  (+ - /)
-;  (* /)
-;  (+ * /)
-;  (- * /)
-;  (+ - * /))
+; prints out '(( (+)(-)(+ -)(*)(+ *)(- *)(+ - *)(/)(+ /)(- /)(+ - /)(* /)(+ * /)(- * /)(+ - * /))
 ;(combinations ops)
 
 ; '((5 25))
@@ -69,43 +35,50 @@
 
 ; prints out '((5 25) (25 5))
 (define num (permutations (list 5 25)))
- num
+;num
 
 ; prints out '((+ (5 25)) (+ (25 5)) (- (5 25)) (- (25 5)) (* (5 25)) (* (25 5)) (/ (5 25)) (/ (25 5)))
 ;(cartesian-product ops num)
 
-; prints out '((+ 5 5)
-;  (+ 5 25)
-;  (+ 25 5)
-;  (+ 25 25)
-;  (- 5 5)
-;  (- 5 25)
-;  (- 25 5)
-;  (- 25 25)
-;  (* 5 5)
-;  (* 5 25)
-;  (* 25 5)
-;  (* 25 25)
-;  (/ 5 5)
-;  (/ 5 25)
-;  (/ 25 5)
-;  (/ 25 25))
+; prints out '((+ 5 5)(+ 5 25)(+ 25 5)(+ 25 25)(- 5 5)(- 5 25)(- 25 5)(- 25 25)(* 5 5)(* 5 25)(* 25 5)(* 25 25)(/ 5 5)(/ 5 25)(/ 25 5)/ 25 25))
 (define allCart (cartesian-product ops (list 5 25) (list 5 25)))
-;allCart
+; allCart
 
 
 ; define namespace to use eval on allCart list
 (define ns (make-base-namespace))
 
-(define (getList l)
+; recursive function that takes in the full combination of numbers and operators list and fills a second list with answers
+(define (getList l a)
   (if (null? l)
-      1
-      (car l)))
+      a
+      (getList (cdr l)(cons (eval (car l) ns) a))))
 
-; prints 10 which is first calculation - need to work on recursion
-(eval (getList allCart) ns)
+; recursive function that checks if target value is in list of answers
+(define (checkTarget l a t)
+  (if (null? l)
+      a
+      (checkTarget (cdr l)(cons (equal? t (car l)) a) t)))
 
-;(getList allCart)
-;(eval (getList allCart))
-;(pair? allCart)
+
+; define function to call that automatically fills in null list for getList
+(define (gList l) (getList l null))
+
+; define variable answers to be the returned list from getList function
+(define answers (gList allCart))
+
+; print answers
+answers
+
+;(define check (checkTarget answers null t))
+;check
+
+(checkTarget answers null t)
+
+; expression finds first example of element and returns it
+;(findf (lambda (arg)
+;              (= arg t))
+;       answers)
+
+
 
